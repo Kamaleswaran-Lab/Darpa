f93744e504da4d4f36caa93a3e458ede92bb6f36
Hassan <mh715@users.noreply.github.com>
2026-01-29 23:21:46 -0500

    - add repo

--- generate_submission.py
+++ generate_submission.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """
+Author: Md Hassanuzzaman
+PhD Student, Duke University
+
 Generate DARPA Challenge Submission Predictions
 
 This script generates predictions on the evaluation dataset in the required
